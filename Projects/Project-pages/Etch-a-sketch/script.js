c1d36167b2d4f6f86421087b65a8d5c7870d74a3
ThomasBittar <thomthomas@gmail.com>
2025-05-25 21:26:52 +0200

    VERSION 1
    Created the main page. Added 3 recent projects (calculator, etch a sketch, epidemic)

---
+++ Projects/Project-pages/Etch-a-sketch/script.js
@@ -0,0 +1,50 @@
+//Declaration des constantes
+
+//QuerySelectors
+const body=document.querySelector("body");
+const btn=document.querySelector("button");
+const header=document.querySelector(".header");
+//Declaration des fonctions
+
+//Creation de la grille. createGrid(height,width); 
+function boxHover(e){
+    console.log("being hovered");
+    e.target.classList.toggle("activated");
+    e.target.setAttribute("style",e.target.getAttribute("style")+"background-color: rgb("+Math.random()*100+"%,"+Math.random()*100+"%,"+Math.random()*100+"%);")
+}
+
+function createGrid(height, width){
+    let oldContainer=document.querySelector(".container");
+    if(oldContainer){
+        console.log("remove");
+        body.removeChild(oldContainer);
+    }
+    let containerDiv=document.createElement("div");
+    containerDiv.classList.add("container");
+    body.insertBefore(containerDiv, header);
+    for(let i=0; i<height*width;i++){
+        let div=document.createElement("div");
+        div.classList.add("gridBox");
+        div.setAttribute("style","height:"+60/height+"vw; width:"+60/width+"vw;")
+        containerDiv.appendChild(div);
+        div.addEventListener("mouseover",boxHover);
+    }
+}
+
+function changeGrid(){
+    let value=0;
+    while(value>100||value<1||typeof value!=="number"){
+        value=window.prompt("Enter a new value for the grid side that is below 100");
+        value=parseInt(value);
+        createGrid(value,value);
+    }
+}
+btn.addEventListener("click",changeGrid);
+
+function main(){
+    createGrid(16,16);
+
+}
+main()
+
+
---
+++ script.js
@@ -0,0 +1,18 @@
+//Query Selectors
+
+const project=document.querySelectorAll(".project")
+
+//Variables
+
+let currentImage=0;
+//Fonction
+
+function changeSlide(){
+    project[currentImage].classList.toggle("active");
+    currentImage=(currentImage+1)%3;
+    project[currentImage].classList.toggle("active");
+    console.log("Execution");
+}
+
+
+setInterval(changeSlide,5000);
