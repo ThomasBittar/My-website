45287217b04b8e1968a382c69f60356e60bfd90f
ThomasBittar <thomthomas@gmail.com>
2025-05-31 12:04:37 +0200

    VERSION 1.4
    Added a header to all pages. Updated epidemic-simultation using buttons to modify parameters/relaunch.

--- Projects/Project-pages/Epidemic-simulation/script.js
+++ Projects/Project-pages/Epidemic-simulation/script.js
@@ -1,28 +1,36 @@
-const n=250;
-const b=0.9;
-const g=0.15;
-const tempsEtude=10000000;
+let n = 250;
+let inf = 10;
+let b = 0.9;
+let g = 0.15;
+const tempsEtude = 10000000;
+
+//Query selectors
+const btnI = document.querySelector(".i");
+const btnN = document.querySelector(".n");
+const btnB = document.querySelector(".b");
+const btnG = document.querySelector(".g");
+const btnR = document.querySelector(".r");
 //Donc R0 = 4/3
 
-let etat={
-    s: n-10,
-    i: 5,
+let etat = {
+    s: n - inf,
+    i: inf,
     tempsEcoule: 0
 }
 
-let va1=0;
+let va1 = 0;
 //Base pour déterminer tempsInter
 
-let va2=0;
+let va2 = 0;
 //Base pour déterminer evenement
 
-let tempsInter=0;
+let tempsInter = 0;
 /*
 Variable aléatoire à loi exponentielle.
 */
 
 
-let evenement=0;        
+let evenement = 0;
 /*
 
 Variable qui prend pour valeurs 
@@ -42,28 +50,56 @@ Calcule l'état suivant
 Paramètres :  Etat actuel
 Retourne : Etat suivant
 */
-function etatSuivant(etat){
+function getI() {
+    let choice = NaN;
+    while (isNaN(choice) || choice <= 0) {
+        choice = Number(window.prompt("Please enter a number greater than 0"));
+    }
+    inf = choice;
+}
+function getN() {
+    let choice = NaN;
+    while (isNaN(choice) || choice <= 0) {
+        choice = Number(window.prompt("Please enter a number greater than 0"));
+    }
+    n = choice;
+}
+function getB() {
+    let choice = NaN;
+    while (isNaN(choice) || choice <= 0) {
+        choice = Number(window.prompt("Please enter a number greater than 0"));
+    }
+    b = choice;
+}
+function getG() {
+    let choice = NaN;
+    while (isNaN(choice) || choice <= 0) {
+        choice = Number(window.prompt("Please enter a number greater than 0"));
+    }
+    g = choice;
+}
+function etatSuivant(etat) {
     //assigner une valeur aléatoire à va1 et va2
-    va1=Math.random();  
-    va2=Math.random();
+    va1 = Math.random();
+    va2 = Math.random();
     //Déduire la valeur de dt
-    tempsInter=-Math.log(va1)/((b*(etat.s)*(etat.i))/n+g*(etat.i));
+    tempsInter = -Math.log(va1) / ((b * (etat.s) * (etat.i)) / n + g * (etat.i));
     //Déduire la valeur de evenement
-    if(va2<((b*(etat.s)*(etat.i)/n)/((b*(etat.s)*(etat.i)/n)+g*(etat.i)))){
-        evenement=0;
+    if (va2 < ((b * (etat.s) * (etat.i) / n) / ((b * (etat.s) * (etat.i) / n) + g * (etat.i)))) {
+        evenement = 0;
     }
     else {
-        evenement=1;
+        evenement = 1;
     }
     //Mettre à jour etat
-    etat.tempsEcoule+=tempsInter;
-    switch(evenement){
+    etat.tempsEcoule += tempsInter;
+    switch (evenement) {
         case 0: //Transition S I
             etat.s--;
             etat.i++;
             break;
         case 1: //Transition I R
-            etat.s=etat.s;
+            etat.s = etat.s;
             etat.i--;
             break;
     }
@@ -99,64 +135,70 @@ function ajouterPoint(etat) {
 
 
 function tracerGraphe(arrayEtats) {
-  const graphe = document.getElementById("graphe");
-  graphe.innerHTML = ""; // vider le graphe
+    const graphe = document.getElementById("graphe");
+    graphe.innerHTML = ""; // vider le graphe
 
-  // Trouver index du premier état où i = 0
-  let idxZero = arrayEtats.findIndex(etat => etat.i === 0);
-  if (idxZero === -1) idxZero = arrayEtats.length - 1;
+    // Trouver index du premier état où i = 0
+    let idxZero = arrayEtats.findIndex(etat => etat.i === 0);
+    if (idxZero === -1) idxZero = arrayEtats.length - 1;
 
-  const tempsMax = arrayEtats[idxZero].tempsEcoule;
-  const largeur = graphe.clientWidth;
-  const hauteur = graphe.clientHeight;
+    const tempsMax = arrayEtats[idxZero].tempsEcoule;
+    const largeur = graphe.clientWidth;
+    const hauteur = graphe.clientHeight;
 
-  // Échelle X
-  const scaleX = largeur / tempsMax;
+    // Échelle X
+    const scaleX = largeur / tempsMax;
 
-  // Calcul max pour i, s et r
-  const maxI = Math.max(...arrayEtats.map(e => e.i));
-  const maxS = Math.max(...arrayEtats.map(e => e.s));
-  const maxR = Math.max(...arrayEtats.map(e => n - (e.s + e.i)));
-  const maxY = Math.max(maxI, maxS, maxR);
+    // Calcul max pour i, s et r
+    const maxI = Math.max(...arrayEtats.map(e => e.i));
+    const maxS = Math.max(...arrayEtats.map(e => e.s));
+    const maxR = Math.max(...arrayEtats.map(e => n - (e.s + e.i)));
+    const maxY = Math.max(maxI, maxS, maxR);
 
-  // Échelle Y
-  const scaleY = hauteur / maxY;
+    // Échelle Y
+    const scaleY = hauteur / maxY;
 
-  // Fonction pour créer un point coloré
-  function creerPoint(x, y, color) {
-    const point = document.createElement("div");
-    point.classList.add("point");
-    point.style.left = `${x}px`;
-    point.style.top = `${y}px`;
-    point.style.backgroundColor = color;
-    graphe.appendChild(point);
-  }
+    // Fonction pour créer un point coloré
+    function creerPoint(x, y, color) {
+        const point = document.createElement("div");
+        point.classList.add("point");
+        point.style.left = `${x}px`;
+        point.style.top = `${y}px`;
+        point.style.backgroundColor = color;
+        graphe.appendChild(point);
+    }
 
-  // Tracer S en bleu, I en rouge, R en vert
-  arrayEtats.forEach(etat => {
-    const x = etat.tempsEcoule * scaleX;
-    const yS = hauteur - (etat.s * scaleY);
-    const yI = hauteur - (etat.i * scaleY);
-    const r = n - (etat.s + etat.i);
-    const yR = hauteur - (r * scaleY);
+    // Tracer S en bleu, I en rouge, R en vert
+    arrayEtats.forEach(etat => {
+        const x = etat.tempsEcoule * scaleX;
+        const yS = hauteur - (etat.s * scaleY);
+        const yI = hauteur - (etat.i * scaleY);
+        const r = n - (etat.s + etat.i);
+        const yR = hauteur - (r * scaleY);
 
-    creerPoint(x, yS, "blue");   // S en bleu
-    creerPoint(x, yI, "red");    // I en rouge
-    creerPoint(x, yR, "green");  // R en vert
-  });
+        creerPoint(x, yS, "blue");   // S en bleu
+        creerPoint(x, yI, "red");    // I en rouge
+        creerPoint(x, yR, "green");  // R en vert
+    });
 }
 
 
 
 
-function main(){
-    let arrayEtats=[{
+function epidemic() {
+    etat = {
+        s: n - inf,
+        i: inf,
+        tempsEcoule: 0
+    };
+
+    let arrayEtats = [{
         s: etat.s,
         i: etat.i,
         tempsEcoule: etat.tempsEcoule
     }];
-    while(etat.tempsEcoule<tempsEtude && etat.i>0){
-        etat=etatSuivant(etat);
+    while (etat.tempsEcoule < tempsEtude && etat.i > 0) {
+        etat = etatSuivant(etat);
         arrayEtats.push({
             s: etat.s,
             i: etat.i,
@@ -166,4 +208,11 @@ function main(){
     console.log(arrayEtats);
     tracerGraphe(arrayEtats);
 }
-main();
+epidemic();
+
+btnI.addEventListener("click", getI);
+btnN.addEventListener("click", getN);
+btnB.addEventListener("click", getB);
+btnG.addEventListener("click", getG);
+btnR.addEventListener("click", epidemic);
+
